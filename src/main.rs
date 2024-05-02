d205d4261a4f464ffcd2933fc828923473c45105
newtbot <limtiseng@gmail.com>
2024-05-02 23:11:08 +0800

    first!

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+/*
+1) function that gets elytra and iron and netherite price
+2) email function
+3) main function???
+*/
+
+// try{
+
+// }catch(error){
+
+// }
+
+//we need to allow our var to be mutable since prices will or may change
+
+
+fn getAPI(){
+
+}
+
+fn sendEmail(){
+
+}
+
+fn main(){
+
+}
